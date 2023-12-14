337c2dace4d5e9a938c6157003456ea9a1001379
nataliag409 <natalia212329@gmail.com>
2023-12-14 12:29:42 -0800

    Update bird.cs

--- FlappyBirdGGutierrezP5/Assets/scripts/bird.cs
+++ FlappyBirdGGutierrezP5/Assets/scripts/bird.cs
@@ -24,7 +24,7 @@ public class bird : MonoBehaviour
             if (Input.GetMouseButtonDown (0))
             {
                 rb2d.velocity = Vector2.zero;
-                rb2d.AddForce(new Vector2(0, upForce));
+                rb2d.AddForce(new Vector2 (0, upForce));
             }
         }
         
